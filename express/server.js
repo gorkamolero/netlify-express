630c2bfd94551fe62cf4d82149320cd5a1fc993e
Gorka <gorka.molero@gmail.com>
2022-02-16 21:01:40 +0100

    openai

--- express/server.js
+++ express/server.js
@@ -1,22 +1,36 @@
-'use strict';
 const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
-const app = express();
-const bodyParser = require('body-parser');
-
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch');
+const app = express()
+const port = 3008
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-module.exports = app;
-module.exports.handler = serverless(app);
+app.post('/api/generate', (req, res) => {
+  var PROMPT = req.body.subject;
+  var raw = JSON.stringify({"prompt": PROMPT, "max_tokens": 5 });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+    },
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch("https://api.openai.com/v1/engines/davinci/completions", requestOptions)
+    .then(response => response.json())
+    .then(json => {
+      res.status(200).json(json);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('An error occurred');
+    })
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
